f66415ee90279cc8391131e17bdb80db297d2260
Bob Oyier <boboyier92@gmail.com>
2022-12-07 08:25:26 +0300

    "done with the task lister lab"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const textbox = document.getElementById("create-task-form");
+textbox.addEventListener("submit", (e) => {
+	e.preventDefault();
+	buildToDo(e.target.new_task_description.value);
 });
+
+function buildToDo(todo) {
+	const paragraph = document.createElement("p");
+	const button = document.createElement("button");
+	button.textContent = "x";
+	paragraph.textContent = `${todo} `;
+
+	document.getElementById("main-content").append(paragraph);
+	paragraph.appendChild(button);
+
+	button.addEventListener("click", deleteAction);
+}
+
+function deleteAction(paragraph) {
+	return paragraph.target.parentNode.remove();
+}
